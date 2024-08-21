89a59c3998199a5957721a60012d20386b89ae10
Rimon Ali <rimonali450@gmail.com>
2024-08-21 20:37:48 +0600

    filter data by category system successfully

--- index.js
+++ index.js
@@ -2,43 +2,118 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
 app.use(express.json())
 
-
+app.get("/", (req, res) => {
+  res.send('My server with pureleaf project app')
+})
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wods307.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const userDataCollection = client.db("PureLeafDB").collection("usersData");
+    const cardDataCollection = client.db("PureLeafDB").collection("cardData");
+
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_JWT_TOKEN_SECRET)
+      res.send({ token })
+    })
+
+    // jwt midleware
+    //   const verifyToken = (req, res, next) => {
+    //     // console.log('inside verify token', req.headers);
+    //     if (!req.headers.authorization) {
+    //       return res.status(401).send({ message: 'unauthorize access' });
+    //     }
+    //     const token = req.headers.authorization.split(' ')[1];
+    //     jwt.verify(token, process.env.ACCESS_JWT_TOKEN_SECRET, (err, decoded) => {
+    //       if (err) {
+    //         return res.status(401).send({ message: 'unauthorize access.can not verify' })
+    //       }
+    //       req.decoded = decoded;
+    //       next();
+    //     })
+
+    //   }
+
+    //   user data send to mongoDb database
+    app.post('/usersData', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userDataCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userDataCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // get all data here
+    // app.get('/cardData', async (req, res) => {
+    //   const result = await cardDataCollection.find().toArray();
+    //   res.send(result);
+    // })
+
+    // data short by category here
+    app.get('/cardData', async (req, res) => {
+      const category = req.query.category;
+      let query = {};
+  
+      if (category && category !== 'null') {
+          query.category = category;
+      }
+  
+      const result = await cardDataCollection.find(query).toArray();
+      res.send(result);
+  });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
 
-app.get("/", (req, res) => {
-    res.send('My server with nodemon app')
-})
+
 app.listen(port, () => {
-    console.log(`server app running on port ${port}`)
+  console.log(`server app running on port ${port}`)
 })
