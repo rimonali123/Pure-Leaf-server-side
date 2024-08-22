93c82e13a5d875c913d5de8d0fcd328eaf50d334
Rimon Ali <rimonali450@gmail.com>
2024-08-23 02:07:18 +0600

    cart item delete function implement successfully

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors({
@@ -100,6 +100,19 @@ app.get('/cartItemData', async(req, res) =>{
   const  result = await cartItemDataCollection.find().toArray();
   res.send(result)
 })
+
+
+
+app.delete('/cartItemData/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+  const result = await cartItemDataCollection.deleteOne(query);
+  res.send(result);
+})
+
+
+
+
 
 
 
