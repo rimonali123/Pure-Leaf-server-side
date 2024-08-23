8ae7ea36d88e018dfde88cd85ec53de6d1cc30be
Rimon Ali <rimonali450@gmail.com>
2024-08-23 10:49:03 +0600

    some additional changes

--- index.js
+++ index.js
@@ -117,8 +117,6 @@ app.delete('/cartItemData/:id', async (req, res) => {
 
 
 
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
