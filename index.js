1f9c3a617bd87e7a5c2f0f99affde852351d85f5
Rimon Ali <rimonali450@gmail.com>
2024-09-01 12:38:43 +0600

    payment intent create & implement successfully

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000
 
 app.use(cors({
@@ -188,6 +189,28 @@ async function run() {
       const result = await userInfoCollection.findOne(query);
       res.send(result);
     })
+
+
+
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, ' intent inside')
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: [
+          "card"
+        ],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+
+    })
+
+
+    
 
 
     // Send a ping to confirm a successful connection
